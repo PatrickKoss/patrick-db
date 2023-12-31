28f021a53af2575b500e54ec3ee74bc00eb46528
Patrick Koss <patrick.koss@mail.schwarz>
2023-12-31 14:34:29 +0100

    implement client cli for server

--- server/src/client.rs
+++ server/src/client.rs
@@ -1,42 +1,124 @@
+use std::env;
+use clap::{Parser, Subcommand};
 use prost_types::Value;
 use prost_types::value::Kind;
+use anyhow::Result;
 
-use key_value_store::GetRequest;
+use key_value_store::{GetRequest, CreateRequest, UpdateRequest, DeleteRequest};
 use key_value_store::key_value_service_client::KeyValueServiceClient;
 
 pub mod key_value_store {
     tonic::include_proto!("server");
 }
 
+#[derive(Subcommand, PartialEq, Debug)]
+enum Action {
+    Add { key: String, value: String },
+    Update { key: String, value: String },
+    Get { key: String },
+    Delete { key: String },
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long, default_value = "http://[::1]:50051")]
+    server_url: String,
+    #[command(subcommand)]
+    action: Action,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = KeyValueServiceClient::connect("http://[::1]:50051").await?;
+    let args = Args::parse();
 
-    let value = Value {
-        kind: Some(Kind::StringValue("value".to_string())),
-    };
+    let server_url = env::var("SERVER_URL").ok().unwrap_or(args.server_url);
+    let mut client = KeyValueServiceClient::connect(server_url).await?;
 
-    let request = tonic::Request::new(GetRequest {
-        key: value.into(),
-    });
+    match args.action {
+        Action::Add { key, value } => {
+            let key = Value {
+                kind: Some(Kind::StringValue(key)),
+            };
+            let value = Value {
+                kind: Some(Kind::StringValue(value)),
+            };
 
-    let response = client.get(request).await?;
+            let request = tonic::Request::new(CreateRequest {
+                key_value: Some(key_value_store::KeyValue {
+                    key: Some(key),
+                    value: Some(value),
+                }),
+            });
 
-    println!("RESPONSE={:?}", response);
+            let response = client.create(request).await?;
+            println!("RESPONSE={:?}", response);
+            let key_value = response.into_inner().key_value.unwrap();
+            print_key_value(key_value);
+        }
+        Action::Get { key } => {
+            let key = Value {
+                kind: Some(Kind::StringValue(key)),
+            };
 
-    let key_value = response.into_inner().key_value.unwrap();
+            let request = tonic::Request::new(GetRequest {
+                key: key.into(),
+            });
 
+            let response = client.get(request).await?;
+            println!("RESPONSE={:?}", response);
+            let key_value = response.into_inner().key_value.unwrap();
+            print_key_value(key_value);
+        }
+        Action::Delete { key } => {
+            let key = Value {
+                kind: Some(Kind::StringValue(key)),
+            };
+
+            let request = tonic::Request::new(DeleteRequest {
+                key: key.into(),
+            });
+
+            let response = client.delete(request).await?;
+            println!("RESPONSE={:?}", response);
+            let key_value = response.into_inner().key_value.unwrap();
+            print_key_value(key_value);
+        }
+        Action::Update { key, value } => {
+            let key = Value {
+                kind: Some(Kind::StringValue(key)),
+            };
+            let value = Value {
+                kind: Some(Kind::StringValue(value)),
+            };
+
+            let request = tonic::Request::new(UpdateRequest {
+                key_value: Some(key_value_store::KeyValue {
+                    key: Some(key),
+                    value: Some(value),
+                }),
+            });
+
+            let response = client.update(request).await?;
+            println!("RESPONSE={:?}", response);
+            let key_value = response.into_inner().key_value.unwrap();
+            print_key_value(key_value);
+        }
+    }
+
+    Ok(())
+}
+
+fn print_key_value(key_value: key_value_store::KeyValue) {
     let key = match key_value.key.unwrap().kind {
         Some(Kind::StringValue(s)) => s,
         _ => String::from(""),
     };
-    println!("VALUE={:?}", &key);
+    println!("KEY={:?}", &key);
 
     let value = match key_value.value.unwrap().kind {
         Some(Kind::StringValue(s)) => s,
         _ => String::from(""),
     };
     println!("VALUE={:?}", &value);
-
-    Ok(())
 }
